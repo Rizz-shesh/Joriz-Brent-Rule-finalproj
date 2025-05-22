d7dd1c63cbdaf50e4bf762138dc5a855514f2ac0
Rizz-shesh <rizz.shesh@gmail.com>
2025-05-22 19:35:17 +0800

    Fix pdfkit and wkhtmltopdf installation

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@
 set -o errexit
 
 # Install system dependencies
-sudo apt-get update
-sudo apt-get install -y wkhtmltopdf xvfb
+apt-get update
+apt-get install -y wget xfonts-75dpi xfonts-base
+
+# Download and install wkhtmltopdf
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-2/wkhtmltox_0.12.6.1-2.jammy_amd64.deb
+dpkg -i wkhtmltox_0.12.6.1-2.jammy_amd64.deb || true
+apt-get install -f -y
+rm wkhtmltox_0.12.6.1-2.jammy_amd64.deb
 
 # Install Python dependencies
 pip install -r requirements.txt
